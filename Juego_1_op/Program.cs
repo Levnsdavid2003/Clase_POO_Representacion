2788041f1fb380a5f8efe4921b4dd349f7bb641b
Levnsdavid2003 <levinezdavid@gmail.com>
2025-05-15 17:41:58 -0300

    Clase_POO_Representacion C#

--- Juego_1_op/Program.cs
+++ Juego_1_op/Program.cs
@@ -8,23 +8,51 @@ namespace Juego_1_op
 {
     internal class Program
     {
+        static Random azar = new Random();
         static void Main(string[] args)
         {
-            int posJugador1, posJugador2;
-            string nombreJ1, nombreJ2;
 
-            posJugador1 = 0;
-            posJugador2 = 0;
+            int dado;
+            bool finJuego = false;
+
+            int posJugador1 = 0;
+            int posJugador2 = 0;
+
 
             Console.WriteLine("Escriba el nombre del Jugador 1 y 2");
-            nombreJ1 = Console.ReadLine();
-            nombreJ2 = Console.ReadLine();
+            string nombreJ1 = Console.ReadLine();
+            string nombreJ2 = Console.ReadLine();
 
 
+            int pieEscalera1 = azar.Next(1, 100);
+            int cabeceraEscalera1 = azar.Next(pieEscalera1, 100);
 
+            int pieEscalera2 = azar.Next(1, 100);
+            int cabeceraEscalera2 = azar.Next(pieEscalera2, 100);
 
+            int serpienteCola = azar.Next(1, 100);
+            int serpienteCabeza = azar.Next(serpienteCola, 100);
 
+            do
+            {
 
+                dado = azar.Next(1, 6);
+                posJugador1 += dado;
+
+                if (posJugador1 == serpienteCabeza) { posJugador1 = serpienteCola; }
+                if (posJugador1 == pieEscalera1) { posJugador1 = cabeceraEscalera1; }
+                if (posJugador1 == pieEscalera2) { posJugador1 = cabeceraEscalera2; }
+
+                dado = azar.Next(1, 6);
+                posJugador2 += dado;
+                if (posJugador2 == serpienteCabeza) { posJugador2 = serpienteCola; }
+                if (posJugador2 == pieEscalera1) { posJugador2 = cabeceraEscalera1; }
+                if (posJugador2 == pieEscalera2) { posJugador2 = cabeceraEscalera2; }
+
+                Console.WriteLine("Posición Jugador 1: " + posJugador1);
+                Console.WriteLine("Posición Jugador 2:" + posJugador2);
+                Console.WriteLine("-------------------- o --------------------");
+            } while (!finJuego);
 
 
 
