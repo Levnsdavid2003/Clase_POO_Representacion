403077a17928ff119294f5587dc23f38c12830b8
Levnsdavid2003 <levinezdavid@gmail.com>
2025-05-15 16:39:27 -0300

    Clase_POO_Representacion C#

--- Juego_1_op/Program.cs
+++ Juego_1_op/Program.cs
@@ -10,6 +10,26 @@ namespace Juego_1_op
     {
         static void Main(string[] args)
         {
+            int posJugador1, posJugador2;
+            string nombreJ1, nombreJ2;
+
+            posJugador1 = 0;
+            posJugador2 = 0;
+
+            Console.WriteLine("Escriba el nombre del Jugador 1 y 2");
+            nombreJ1 = Console.ReadLine();
+            nombreJ2 = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
